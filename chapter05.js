37fff16dd41977c026a33a691be400170c13bfd8
Seono-Na <seono5670@gmail.com>
2025-05-01 17:06:09 +0900

    chapter05. 자료형

---
+++ chapter05.js
@@ -0,0 +1,27 @@
+// 1. Number Type
+let num1 = 27;
+let num2 = 2.3;
+let num3 = -18;
+
+let inf = Infinity;
+let mInf = -Infinity;
+
+let nan = NaN;
+console.log(1 * "Hello");
+
+// 2. String Type
+let myName = "홍길동";
+let myLocation = "서울";
+let introduce = myName + myLocation;
+
+let introduceText = `${myName}은 ${myLocation}에 거주합니다`;
+
+// 3. Boolean Type
+let isSwitchOn = true;
+let isEmpty = false;
+
+// 4. Null Type (아무것도 없다)
+let empty = null;
+
+// 5. Undefined Type
+let none;
